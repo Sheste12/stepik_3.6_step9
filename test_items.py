82eb4e56074137c98263b3144bcc86341aa24a02
Eugeny Shesteperov <sheste12@yandex.ru>
2020-03-26 16:59:02 +0300

    Edit time.sleep() for review

--- test_items.py
+++ test_items.py
@@ -10,6 +10,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_items(browser):
     browser.get(link)
+    # time.sleep(30)
     button = WDW(browser, 5).until(EC.visibility_of_all_elements_located( (By.CSS_SELECTOR, ".btn-add-to-basket") ))
     assert 1 == len(button), "Button 'Add to basket' is not found or is not unique button."
-    time.sleep(5)
