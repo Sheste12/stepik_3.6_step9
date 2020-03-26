8a482775b995312aa9057b269dd9566c0105f539
sheste12 <sheste12@gmail.com>
2020-03-26 16:56:18 +0300

    Create a test for tasks

---
+++ conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption("--language", action="store", default="en", help="Choose your language.")
+    parser.addoption("--browser", action="store", default="chrome", help="Choose your browser: chrome of firefox")
+
+# Default values:
+# Language: English
+# Browser:  Chrome
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_language = request.config.getoption("language")
+    browser_name = request.config.getoption("browser")
+    if browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", browser_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        browser = webdriver.Chrome(options=opt)
+        opt = Options()
+        opt.add_xexperimental_option("prefs", {'intl.accept_languages': browser_language})
+    yield browser
+    browser.quit()
---
+++ test_items.py
@@ -0,0 +1,15 @@
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait as WDW
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import pytest
+import selenium
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_items(browser):
+    browser.get(link)
+    button = WDW(browser, 5).until(EC.visibility_of_all_elements_located( (By.CSS_SELECTOR, ".btn-add-to-basket") ))
+    assert 1 == len(button), "Button 'Add to basket' is not found or is not unique button."
+    time.sleep(5)
